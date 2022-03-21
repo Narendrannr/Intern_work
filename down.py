035387908973ac9b88a8bf3d7d386ff359560720
narendran <42309607+Narendrannr@users.noreply.github.com>
2022-03-21 21:53:25 +0530

    EC2-Task

---
+++ down.py
@@ -0,0 +1,15 @@
+import boto3
+import botocore
+
+BUCKET_NAME = 'testbucket30' # replace with your bucket name
+KEY = 'batman.jpg' # replace with your object key
+
+s3 = boto3.resource(service_name='s3',region_name ='us-east-1',aws_access_key_id='AKIAQMWN6KTYKWWLAD7S',aws_secret_access_key='hs4c5W3488Pq/Zsv1tuYlREpJIlyjn/6qg+oSH8n')
+# download a file stored in s3 bucket from ec2 instance.
+try:
+    s3.Bucket(BUCKET_NAME).download_file(KEY, 'bat.jpg')
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
